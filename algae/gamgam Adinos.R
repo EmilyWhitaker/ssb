d6e1cd8a9e8f040797157bd937034355a37750dd
EmilyWhitaker <ewhitaker524@gmail.com>
2020-02-20 13:45:12 -0600

    wip

---
+++ algae/gamgam Adinos.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(mgcv)
+
+Adinosyears <- read_csv("Adino_lite3_chloro_datayearonly.csv", 
+                       col_types = cols(X1 = col_skip(), X1.x = col_skip(), 
+                                        daynum = col_skip(), sampledate = col_date(format = "%m/%d/%Y")))
+View(Adinosyears)
+
+##Add in daynumber        
+Adinosyears$daynum<- yday(Adinosyears$sampledate)
+Adinosyears
+
+Adinosexperience <- gam(CellBioVol ~ s(year4.x, bs = "gp")+ s(daynum), data=Adinosyears)
+Ad<-plot(Adinosexperience, page=1)
+Ad
+summary(Adinosexperience)
