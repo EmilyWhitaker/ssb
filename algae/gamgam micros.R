7360b4d558b3e6f15690e35d47a879478d8aff1f
EmilyWhitaker <ewhitaker524@gmail.com>
2020-02-22 14:11:38 -0600

    wip

--- algae/gamgam micros.R
+++ algae/gamgam micros.R
@@ -14,6 +14,8 @@ Microcystisyears
 
 Microcystisexperience <- gam(CellBioVol ~ s(year4.x, bs = "gp")+ s(daynum), data=Microcystisyears)
 mirco<-plot(Microcystisexperience, page=1)
+mirco<- plot(Microcystisexperience,pages=1,residuals=TRUE,all.terms=TRUE, pch=16)
+
 micro
 summary(Microcystisexperience)
 
