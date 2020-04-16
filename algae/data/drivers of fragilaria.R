3465a950d823252e62c49f74cd67a27fbd249d60
EmilyWhitaker <ewhitaker524@gmail.com>
2020-04-16 09:49:10 -0500

    update

--- algae/data/drivers of fragilaria.R
+++ algae/data/drivers of fragilaria.R
@@ -19,408 +19,408 @@ totals_and_genus$int.cbv=as.integer(totals_and_genus$CellBioVol)
 totals_and_genus$oneint.cbv = totals_and_genus$int.cbv+1
 totals_and_genus$logint.cbv =as.integer(log(totals_and_genus$oneint.cbv))
 
-#gen.keep = c( "Dinobryon", "Fragilaria")
+#gen.keep = c( "Fragilaria")
 
 #=====
-#Dinobryon during ice-on
+#Fragilaria during ice-on
 
 ice.on = subset(totals_and_genus, ice.pres == 1)
-gen.keep1=c("Dinobryon")
+gen.keep1=c("Fragilaria")
 genus.sub1 = subset(totals_and_genus, Genus %in% gen.keep1)
-Dino.ice.on = subset(genus.sub1, ice.pres == 1) #correct
+Frag.ice.on = subset(genus.sub1, ice.pres == 1) #correct
 
-hist(Dino.ice.on$log.cbv) #zero-inflated!!!!!!
+hist(Frag.ice.on$log.cbv) #zero-inflated!!!!!!
 
 #===== 
 #Linear graphs Ice on
-ggplot(Dino.ice.on, aes(wtemp, log.cbv))+
+ggplot(Frag.ice.on, aes(wtemp, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(o2, log.cbv))+
+ggplot(Frag.ice.on, aes(o2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(o2sat, log.cbv))+
+ggplot(Frag.ice.on, aes(o2sat, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(cond, log.cbv))+
+ggplot(Frag.ice.on, aes(cond, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(frlight, log.cbv))+
+ggplot(Frag.ice.on, aes(frlight, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(chlor.int, log.cbv))+
+ggplot(Frag.ice.on, aes(chlor.int, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(phaeo, log.cbv))+
+ggplot(Frag.ice.on, aes(phaeo, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(ph, log.cbv))+
+ggplot(Frag.ice.on, aes(ph, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(phair, log.cbv))+
+ggplot(Frag.ice.on, aes(phair, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(alk, log.cbv))+
+ggplot(Frag.ice.on, aes(alk, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(dic, log.cbv))+
+ggplot(Frag.ice.on, aes(dic, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(tic, log.cbv))+
+ggplot(Frag.ice.on, aes(tic, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(doc, log.cbv))+
+ggplot(Frag.ice.on, aes(doc, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(toc, log.cbv))+
+ggplot(Frag.ice.on, aes(toc, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(no3no2, log.cbv))+
+ggplot(Frag.ice.on, aes(no3no2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(no2, log.cbv))+
+ggplot(Frag.ice.on, aes(no2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(nh4, log.cbv))+
+ggplot(Frag.ice.on, aes(nh4, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(totnf, log.cbv))+
+ggplot(Frag.ice.on, aes(totnf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(totnuf, log.cbv))+
+ggplot(Frag.ice.on, aes(totnuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(totpf, log.cbv))+
+ggplot(Frag.ice.on, aes(totpf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(totpuf, log.cbv))+
+ggplot(Frag.ice.on, aes(totpuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(drsif, log.cbv))+
+ggplot(Frag.ice.on, aes(drsif, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(brsif, log.cbv))+
+ggplot(Frag.ice.on, aes(brsif, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(brsiuf, log.cbv))+
+ggplot(Frag.ice.on, aes(brsiuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(tpm, log.cbv))+
+ggplot(Frag.ice.on, aes(tpm, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(cl, log.cbv))+
+ggplot(Frag.ice.on, aes(cl, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(so4, log.cbv))+
+ggplot(Frag.ice.on, aes(so4, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(ca, log.cbv))+
+ggplot(Frag.ice.on, aes(ca, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(mg, log.cbv))+
+ggplot(Frag.ice.on, aes(mg, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(na, log.cbv))+
+ggplot(Frag.ice.on, aes(na, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(k, log.cbv))+
+ggplot(Frag.ice.on, aes(k, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(fe, log.cbv))+
+ggplot(Frag.ice.on, aes(fe, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(mn, log.cbv))+
+ggplot(Frag.ice.on, aes(mn, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(chlor.surf, log.cbv))+
+ggplot(Frag.ice.on, aes(chlor.surf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(avsnow, log.cbv))+
+ggplot(Frag.ice.on, aes(avsnow, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(totice, log.cbv))+
+ggplot(Frag.ice.on, aes(totice, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(whiteice, log.cbv))+
+ggplot(Frag.ice.on, aes(whiteice, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(blueice, log.cbv))+
+ggplot(Frag.ice.on, aes(blueice, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(light, log.cbv))+
+ggplot(Frag.ice.on, aes(light, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.on, aes(iceduration, log.cbv))+
+ggplot(Frag.ice.on, aes(iceduration, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
 #=======
-#Dinobryon ice off
+#Fragilaria ice off
 
 ice.off = subset(totals_and_genus, ice.pres == 0)
-gen.keep1=c("Dinobryon")
+gen.keep1=c("Fragilaria")
 genus.sub1 = subset(totals_and_genus, Genus %in% gen.keep1)
-Dino.ice.off = subset(genus.sub1, ice.pres == 0) #correct
+Frag.ice.off = subset(genus.sub1, ice.pres == 0) #correct
 
-hist(Dino.ice.off$log.cbv) 
+hist(Frag.ice.off$log.cbv) #zero inflated  
 
 #=======
 #Linear graphs Ice off
-ggplot(Dino.ice.off, aes(wtemp, log.cbv))+
+ggplot(Frag.ice.off, aes(wtemp, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(o2, log.cbv))+
+ggplot(Frag.ice.off, aes(o2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(o2sat, log.cbv))+
+ggplot(Frag.ice.off, aes(o2sat, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(cond, log.cbv))+
+ggplot(Frag.ice.off, aes(cond, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(frlight, log.cbv))+
+ggplot(Frag.ice.off, aes(frlight, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(chlor.int, log.cbv))+
+ggplot(Frag.ice.off, aes(chlor.int, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F)+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(phaeo, log.cbv))+
+ggplot(Frag.ice.off, aes(phaeo, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(ph, log.cbv))+
+ggplot(Frag.ice.off, aes(ph, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(phair, log.cbv))+
+ggplot(Frag.ice.off, aes(phair, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(alk, log.cbv))+
+ggplot(Frag.ice.off, aes(alk, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(dic, log.cbv))+
+ggplot(Frag.ice.off, aes(dic, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(tic, log.cbv))+
+ggplot(Frag.ice.off, aes(tic, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(doc, log.cbv))+
+ggplot(Frag.ice.off, aes(doc, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(toc, log.cbv))+
+ggplot(Frag.ice.off, aes(toc, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(no3no2, log.cbv))+
+ggplot(Frag.ice.off, aes(no3no2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(no2, log.cbv))+
+ggplot(Frag.ice.off, aes(no2, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(nh4, log.cbv))+
+ggplot(Frag.ice.off, aes(nh4, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(totnf, log.cbv))+
+ggplot(Frag.ice.off, aes(totnf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(totnuf, log.cbv))+
+ggplot(Frag.ice.off, aes(totnuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(totpf, log.cbv))+
+ggplot(Frag.ice.off, aes(totpf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(totpuf, log.cbv))+
+ggplot(Frag.ice.off, aes(totpuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(drsif, log.cbv))+
+ggplot(Frag.ice.off, aes(drsif, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(brsif, log.cbv))+
+ggplot(Frag.ice.off, aes(brsif, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(brsiuf, log.cbv))+
+ggplot(Frag.ice.off, aes(brsiuf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(tpm, log.cbv))+
+ggplot(Frag.ice.off, aes(tpm, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(cl, log.cbv))+
+ggplot(Frag.ice.off, aes(cl, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(so4, log.cbv))+
+ggplot(Frag.ice.off, aes(so4, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(ca, log.cbv))+
+ggplot(Frag.ice.off, aes(ca, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(mg, log.cbv))+
+ggplot(Frag.ice.off, aes(mg, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(na, log.cbv))+
+ggplot(Frag.ice.off, aes(na, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(k, log.cbv))+
+ggplot(Frag.ice.off, aes(k, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(fe, log.cbv))+
+ggplot(Frag.ice.off, aes(fe, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(mn, log.cbv))+
+ggplot(Frag.ice.off, aes(mn, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(chlor.surf, log.cbv))+
+ggplot(Frag.ice.off, aes(chlor.surf, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(light, log.cbv))+
+ggplot(Frag.ice.off, aes(light, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
 
-ggplot(Dino.ice.off, aes(iceduration, log.cbv))+
+ggplot(Frag.ice.off, aes(iceduration, log.cbv))+
   geom_point()+
   geom_smooth(method='lm',se=F, aes(group=Genus))+
   scale_color_brewer(palette = 'Paired')
