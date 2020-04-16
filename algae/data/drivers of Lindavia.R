a239b125527e3f8215d3629a8539262e3b52f8c5
EmilyWhitaker <ewhitaker524@gmail.com>
2020-04-16 09:08:05 -0500

    updates

---
+++ algae/data/drivers of Lindavia.R
@@ -0,0 +1,22 @@
+## Clear workspace and console
+rm(list=ls()); cat("\014")
+
+# Load required packages
+library(FSA); library(dplyr);library(magrittr);library(tidyr) # data management
+library(mgcv);library(nlme); library(lme4) # modeling
+library(viridisLite); library(gridExtra); library(ggplot2) # data viz 
+library(lubridate) # dealing with dates
+library(pscl) #for 2 hurdle modeling
+
+#=========
+totals_and_genus = read.csv('data/clean_abiotic_genus_03262020.csv', stringsAsFactors = F)
+totals_and_genus$sampledate = mdy(totals_and_genus$sampledate)
+
+#add those cols 
+totals_and_genus$one.cbv = totals_and_genus$CellBioVol+1
+totals_and_genus$log.cbv = log(totals_and_genus$one.cbv)
+totals_and_genus$int.cbv=as.integer(totals_and_genus$CellBioVol)
+totals_and_genus$oneint.cbv = totals_and_genus$int.cbv+1
+totals_and_genus$logint.cbv =as.integer(log(totals_and_genus$oneint.cbv))
+
+#gen.keep = c("Lindavia" "Cryptomonad", "Asterionella", "Dinobryon", "Fragilaria")
