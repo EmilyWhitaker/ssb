5d46f8b75eb3d52303a7f8e859ad5909368008d0
EmilyWhitaker <ewhitaker524@gmail.com>
2020-04-27 15:36:40 -0500

    timeseries

--- algae/data/code/old/eda_2_03032020.R
+++ algae/data/code/old/eda_2_03032020.R
@@ -526,7 +526,7 @@ gen.main.total = c("Armored Dinoflagellate","Naked Dinoflagellate","Limnothrix",
 #Asterocapsa-- NOT ENOUGH DATA
 #DONT LOOK @ Cyanobacteria in WINTER
 
-gen.ndmi=c("Lindavia","Cocconeis")
+gen.ndmi=c("Lindavia","Segmented Green")
 
 ggplot(subset(genus2, Genus %in%gen.ndmi), aes(sampledate, log(CellBioVol), color=Genus))+
   geom_vline(data=ice, aes(xintercept=ice.on), linetype='dashed')+
