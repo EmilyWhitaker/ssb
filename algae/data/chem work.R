b1f992202ea79a02db233a5b3d1f073c2a3fb802
EmilyWhitaker <ewhitaker524@gmail.com>
2020-05-08 10:58:02 -0500

    chlor-a

--- algae/data/chem work.R
+++ algae/data/chem work.R
@@ -128,18 +128,18 @@ gen.main.total = c("Armored Dinoflagellate","Naked Dinoflagellate","Limnothrix",
                    "Cryptomonad(DONE)", "Asterionella", "Dinobryon", "Fragilaria","Cocconeis",
                    "Flagellated Green", "Cf. Komvophoron / Trichormus","Cyanobacteria","Elakatothrix","Mallomonas",
                    "Segmented Green", "Peanut","Other")
-gen.ndmi=c("Asterionella")
+gen.ndmi=c("Dinobryon")
 
 ggplot((subset(totals.new, Genus %in%gen.ndmi)), aes(sampledate,log(CellBioVol)))+
   geom_point(aes(col=chlor.int), size=2)+
-  geom_smooth(method=lm)+
-  labs(title="Asterionella Integrated Chlorophyll")+
+  geom_smooth(method=lm,se=T)+
+  labs(title="Dinobryon Integrated Chlorophyll")+
   facet_wrap(~ice.pres, labeller=labeller(ice.pres = ice.labs))
 
 ggplot((subset(totals.new, Genus %in%gen.ndmi)), aes(sampledate,log(CellBioVol)))+
   geom_point(aes(col=chlor.int), size=2)+
-  geom_smooth()+
-  labs(title="Asterionella Integrated Chlorophyll")+
+  geom_smooth(se=F)+
+  labs(title="Dinobryon Integrated Chlorophyll")+
   facet_wrap(~ice.pres, labeller=labeller(ice.pres = ice.labs))
 
 
