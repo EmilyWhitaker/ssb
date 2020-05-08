ef6d9b84d072bffb67cbf8e2de821ac7fff1213f
EmilyWhitaker <ewhitaker524@gmail.com>
2020-05-08 11:15:54 -0500

    chlor-a

--- algae/data/chem work.R
+++ algae/data/chem work.R
@@ -128,18 +128,18 @@ gen.main.total = c("Armored Dinoflagellate","Naked Dinoflagellate","Limnothrix",
                    "Cryptomonad(DONE)", "Asterionella", "Dinobryon", "Fragilaria","Cocconeis",
                    "Flagellated Green", "Cf. Komvophoron / Trichormus","Cyanobacteria","Elakatothrix","Mallomonas",
                    "Segmented Green", "Peanut","Other")
-gen.ndmi=c("Cocconeis")
+gen.ndmi=c("Cf. Komvophoron / Trichormus")
 
 ggplot((subset(totals.new, Genus %in%gen.ndmi)), aes(sampledate,log(CellBioVol)))+
   geom_point(aes(col=chlor.int), size=2)+
   geom_smooth(method=lm,se=T)+
-  labs(title="Cocconeis Integrated Chlorophyll")+
+  labs(title="Cf. Komvophoron / Trichormus Integrated Chlorophyll")+
   facet_wrap(~ice.pres, labeller=labeller(ice.pres = ice.labs))
 
 ggplot((subset(totals.new, Genus %in%gen.ndmi)), aes(sampledate,log(CellBioVol)))+
   geom_point(aes(col=chlor.int), size=2)+
-  geom_smooth(se=T)+
-  labs(title="Cocconeis Integrated Chlorophyll")+
+  geom_smooth(se=F)+
+  labs(title="Cf. Komvophoron / Trichormus Integrated Chlorophyll")+
   facet_wrap(~ice.pres, labeller=labeller(ice.pres = ice.labs))
 
 
