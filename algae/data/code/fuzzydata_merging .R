3bad0f718602a51e5e461b23b2434a150a1fb136
EmilyWhitaker <ewhitaker524@gmail.com>
2020-05-18 20:33:25 -0500

    re-formatting

--- algae/data/code/fuzzydata_merging .R
+++ algae/data/code/fuzzydata_merging .R
@@ -47,22 +47,28 @@ intchems %>%
   rename(chlor.int= chlor)
 
 
-#intchems2= data.frame(intchemsDate = c(intchems$sampledate),
-#                     intchems$wtemp, intchems$o2, intchems$o2sat, intchems$cond, intchems$frlight,
-#                     intchems$chlor, intchems$phaeo, intchems$ph, intchems$phair, intchems$alk,
-#                     intchems$dic, intchems$tic, intchems$doc, intchems$toc, intchems$no3no2,
-#                     intchems$no2, intchems$nh4, intchems$totnf, intchems$totnuf, intchems$totpf,
-#                     intchems$totpuf, intchems$drsif, intchems$brsif, intchems$brsiuf, intchems$tpm,
-#                     intchems$cl, intchems$so4, intchems$ca, intchems$mg, intchems$na, intchems$k,
-#                     intchems$fe, intchems$mn)
+intchems2= data.frame(intchemsDate = c(intchems$sampledate),
+                     intchems$wtemp, intchems$o2, intchems$o2sat, intchems$cond, intchems$frlight,
+                     intchems$chlor, intchems$phaeo, intchems$ph, intchems$phair, intchems$alk,
+                     intchems$dic, intchems$tic, intchems$doc, intchems$toc, intchems$no3no2,
+                     intchems$no2, intchems$nh4, intchems$totnf, intchems$totnuf, intchems$totpf,
+                     intchems$totpuf, intchems$drsif, intchems$brsif, intchems$brsiuf, intchems$tpm,
+                     intchems$cl, intchems$so4, intchems$ca, intchems$mg, intchems$na, intchems$k,
+                     intchems$fe, intchems$mn)
 chl2=data.frame(chlDate= c(chl$sampledate),chl$chlor)%>% 
   mutate(datePlus1 = chlDate + 1) %>% mutate(dateMinus1 = chlDate - 1)
 join <-fuzzy_left_join(intchems2, chl2, by = c("intchemsDate" = "datePlus1", "intchemsDate" = "dateMinus1"), 
                 match_fun = list(`<=`, `>=`))
 
+join %<>% rename(sampledate = intchemsDate)
 
 
 #still need light, ice, and bio vol info 
+
+join_ice <- left_join(join, ice, by= c('sampledate'))
+
+
+
 
 #ice2= data.frame(iceDate = c(ice$sampledate),ice$avsnow, ice$totice, ice$whiteice, ice$blueice)
 
