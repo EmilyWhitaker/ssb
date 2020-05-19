8937b5c79ba46db9b797a1e2b7072c9ae354e892
EmilyWhitaker <ewhitaker524@gmail.com>
2020-05-19 08:43:48 -0500

    re-formatting

--- algae/data/code/fuzzydata_merging .R
+++ algae/data/code/fuzzydata_merging .R
@@ -57,6 +57,9 @@ intchems2= data.frame(intchemsDate = c(intchems$sampledate),
                      intchems$fe, intchems$mn)
 chl2=data.frame(chlDate= c(chl$sampledate),chl$chlor)%>% 
   mutate(datePlus1 = chlDate + 1) %>% mutate(dateMinus1 = chlDate - 1)
+
+
+
 join <-fuzzy_left_join(intchems2, chl2, by = c("intchemsDate" = "datePlus1", "intchemsDate" = "dateMinus1"), 
                 match_fun = list(`<=`, `>=`))
 
@@ -66,12 +69,46 @@ join %<>% rename(sampledate = intchemsDate)
 #still need light, ice, and bio vol info 
 
 join_ice <- left_join(join, ice, by= c('sampledate'))
+join_ice$join_iceDate = ymd(join_ice$join_iceDate)
+join_ice$join_ice$datePlus1 = ymd(join_ice$'join_ice$datePlus1')
+join_ice$join_ice$dateMinus1 = ymd(join_ice$'join_ice$dateMinus1')
 
-#need light and bv, we have fr.light 
+#need light, bv, and surf.chlors we have fr.light 
+
+surfchlor = read.csv('../../chloro_all.csv',stringsAsFactors = F)
+surfchlor %<>% subset(lakeid == "SP" & depth == 0) %>%
+  select(lakeid, sampledate, depth, chlor)
+surfchlor$sampledate = ymd(surfchlor$sampledate)
+
+
+surfchlor2=data.frame(surfchlorDate= c(surfchlor$sampledate),surfchlor$chlor)%>% 
+  mutate(datePlus1 = surfchlorDate + 1) %>% mutate(dateMinus1 = surfchlorDate - 1)
+
+join_ice= data_frame(join_iceDate = c(join_ice$sampledate),
+                     join_ice$intchems.wtemp, join_ice$intchems.o2, join_ice$intchems.o2sat,
+                     join_ice$intchems.cond, join_ice$intchems.frlight,
+                     join_ice$intchems.chlor, join_ice$intchems.phaeo, join_ice$intchems.ph,
+                     join_ice$intchems.phair, join_ice$intchems.alk,
+                     join_ice$intchems.dic, join_ice$intchems.tic, join_ice$intchems.doc,
+                     join_ice$intchems.toc, join_ice$intchems.no3no2,
+                     join_ice$intchems.no2, join_ice$intchems.nh4, join_ice$intchems.totnf,
+                     join_ice$intchems.totnuf, join_ice$intchems.totpf,
+                     join_ice$intchems.totpuf, join_ice$intchems.drsif, join_ice$intchems.brsif,
+                     join_ice$intchems.brsiuf, join_ice$intchems.tpm,
+                     join_ice$intchems.cl, join_ice$intchems.so4, join_ice$intchems.ca, join_ice$intchems.mg, 
+                     join_ice$intchems.na, join_ice$intchems.k,
+                     join_ice$intchems.fe, join_ice$intchems.mn, join_ice$chlDate, join_ice$chl.chlor,
+                     join_ice$avsnow, join_ice$totice, join_ice$whiteice, join_ice$datePlus1, join_ice$dateMinus1)
+
+join_surfchlor <-fuzzy_left_join(join_ice, surfchlor, by = c("surfchlorDate" = "datePlus1", "surfchlorDate" = "dateMinus1"), 
+                       match_fun = list(`<=`, `>=`))
 
 
 
-join_phytos
+totalbv = read.csv("../TotalBVs.csv", stringsAsFactors = F)
+totalbv$sampledate = ymd(totalbv$sampledate)
+totalbv %<>% select(sampledate, Genus, CellBioVol)
+
 
 
 
