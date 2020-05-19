9ef19accbe5b9434728bb3e6b78809ed2bf500c2
EmilyWhitaker <ewhitaker524@gmail.com>
2020-05-19 10:25:16 -0500

    re-formatting

--- algae/data/code/fuzzydata_merging .R
+++ algae/data/code/fuzzydata_merging .R
@@ -9,7 +9,7 @@ library(lubridate) # dealing with dates
 library(ggpubr); library(fuzzyjoin)
 
 ######====
-
+#Hilary's example 
 chlorophyll = data.frame(chloroDate = c(as.Date('2000-01-30'), as.Date('2003-01-30'), 
                                         as.Date('2004-01-30'), as.Date('2006-01-30')), 
                          num = c(23,12,16,18))
@@ -21,31 +21,19 @@ phytos = data.frame(phytoDate = c(as.Date('2000-01-30'), as.Date('2003-01-29'), 
 fuzzy_left_join(chlorophyll, phytos, by = c("chloroDate" = "datePlus1", "chloroDate" = "dateMinus1"), 
                 match_fun = list(`<=`, `>=`))
 
-
-
-ice = read.csv('data/snowicedepth.csv',stringsAsFactors = F)
-ice %<>% subset(lakeid == "SP") %>%
-  select(year4, daynum, sampledate, avsnow, totice, whiteice, blueice) %>%
-  mutate(perwhiteice=(whiteice/totice)*100, 
-         perblueice=(blueice/totice)*100)
-ice$sampledate = ymd(ice$sampledate)
-
+#######
 
 chl = read.csv('data/chloro_all.csv',stringsAsFactors = F)
 chl %<>% subset(lakeid == "SP" & depth == 0) %>%
   select(year4, daynum, sampledate, chlor)
 chl$sampledate =  ymd(chl$sampledate)
-ice2= data.frame(iceDate = c(ice$sampledate))
 
-#phytos 
 #Inegrated Chems 
 intchems = read.csv("data/SPFullChem.csv", stringsAsFactors = F)
 intchems$sampledate = mdy(intchems$sampledate)
 intchems %<>% subset(lakeid == "SP")
 intchems$frlight[intchems$frlight=="1"] <- NA #one iceon point with no light point to calc frlight against 
-intchems %>%
-  rename(chlor.int= chlor)
-
+intchems 
 
 intchems2= data.frame(intchemsDate = c(intchems$sampledate),
                      intchems$wtemp, intchems$o2, intchems$o2sat, intchems$cond, intchems$frlight,
@@ -58,31 +46,50 @@ intchems2= data.frame(intchemsDate = c(intchems$sampledate),
 chl2=data.frame(chlDate= c(chl$sampledate),chl$chlor)%>% 
   mutate(datePlus1 = chlDate + 1) %>% mutate(dateMinus1 = chlDate - 1)
 
-
-
 join <-fuzzy_left_join(intchems2, chl2, by = c("intchemsDate" = "datePlus1", "intchemsDate" = "dateMinus1"), 
                 match_fun = list(`<=`, `>=`))
+
+
+#join3 <- fuzzy_left_join(join, totalbv2, by = c("intchemsDate" = "datePlus1", "intchemsDate" = "dateMinus1"), 
+#                       match_fun = list(`<=`, `>=`)) 
 
 join %<>% rename(sampledate = intchemsDate)
 
 
 #still need light, ice, and bio vol info 
+ice = read.csv('data/snowicedepth.csv',stringsAsFactors = F)
+ice %<>% subset(lakeid == "SP") %>%
+  select(year4, daynum, sampledate, avsnow, totice, whiteice, blueice) 
+ice$sampledate = ymd(ice$sampledate)
 
 join_ice <- left_join(join, ice, by= c('sampledate'))
-join_ice$join_iceDate = ymd(join_ice$join_iceDate)
-join_ice$join_ice$datePlus1 = ymd(join_ice$'join_ice$datePlus1')
-join_ice$join_ice$dateMinus1 = ymd(join_ice$'join_ice$dateMinus1')
 
 #need light, bv, and surf.chlors we have fr.light 
-
+###### 
+#not working 
 surfchlor = read.csv('../../chloro_all.csv',stringsAsFactors = F)
 surfchlor %<>% subset(lakeid == "SP" & depth == 0) %>%
   select(lakeid, sampledate, depth, chlor)
 surfchlor$sampledate = ymd(surfchlor$sampledate)
 
 
-surfchlor2=data.frame(surfchlorDate= c(surfchlor$sampledate),surfchlor$chlor)%>% 
+totalbv = read.csv("../TotalBVs.csv", stringsAsFactors = F)
+totalbv$sampledate = ymd(totalbv$sampledate)
+totalbv %<>% select(sampledate, Genus, CellBioVol)
+
+
+surfchlor2=data.frame(surfchlorDate= c(as.Date(surfchlor$sampledate),surfchlor= c(surfchlor$chlor)))%>% 
   mutate(datePlus1 = surfchlorDate + 1) %>% mutate(dateMinus1 = surfchlorDate - 1)
+
+totalbv2 = data.frame(totalbvDate= c(as.Date(totalbv$sampledate)), genus=c(totalbv$Genus), CellBioVol=c(totalbv$CellBioVol))
+
+
+surftotalbv<- fuzzy_left_join(surfchlor2, totalbv2, by = c("totalbvDate" = "datePlus1", "totalbvDate" = "dateMinus1"), 
+                         match_fun = list(`<=`, `>=`)) 
+
+
+
+
 
 join_ice= data_frame(join_iceDate = c(join_ice$sampledate),
                      join_ice$intchems.wtemp, join_ice$intchems.o2, join_ice$intchems.o2sat,
@@ -103,11 +110,23 @@ join_ice= data_frame(join_iceDate = c(join_ice$sampledate),
 join_surfchlor <-fuzzy_left_join(join_ice, surfchlor, by = c("surfchlorDate" = "datePlus1", "surfchlorDate" = "dateMinus1"), 
                        match_fun = list(`<=`, `>=`))
 
-
+#######====
 
 totalbv = read.csv("../TotalBVs.csv", stringsAsFactors = F)
 totalbv$sampledate = ymd(totalbv$sampledate)
 totalbv %<>% select(sampledate, Genus, CellBioVol)
+
+
+totalbv2 = data.frame(totalbvDate= c(totalbv$sampledate), totalbv$Genus, totalbv$CellBioVol)
+
+
+join= data.frame(joinDate= c(join$sampledate, join$intchems.wtemp)%>% 
+                   mutate(datePlus1 = joinDate + 1) %>% mutate(dateMinus1 = joinDate - 1))
+
+
+join3 <- fuzzy_left_join(join, totalbv2, by = c("totalbvDate" = "datePlus1", "totalbvDate" = "dateMinus1"), 
+                         match_fun = list(`<=`, `>=`)) 
+
 
 join_ice_bv <- fuzzy_left_join(join_ice, totalbv, by= "sampledate", 
                                                         match_fun = list(`<=`, `>=`))
@@ -115,23 +134,12 @@ join_ice_bv <- fuzzy_left_join(join_ice, totalbv, by= "sampledate",
 
 
 
-#ice2= data.frame(iceDate = c(ice$sampledate),ice$avsnow, ice$totice, ice$whiteice, ice$blueice)
+ice2= data.frame(iceDate = c(ice$sampledate),ice$avsnow, ice$totice, ice$whiteice, ice$blueice)
 
 #join2<- fuzzy_left_join(join, ice2, by = c("intchemsDate" = "datePlus1", "intchemsDate" = "dateMinus1"), 
 #                        match_fun = list(`<=`, `>=`))
 
 
 
-
-chlorophyll = data.frame(chloroDate = c(as.Date('2000-01-30'), as.Date('2003-01-30'), 
-                                        as.Date('2004-01-30'), as.Date('2006-01-30')), 
-                         num = c(23,12,16,18))
-# Create columns of date range. In this case, date + 1, and date - 1
-phytos = data.frame(phytoDate = c(as.Date('2000-01-30'), as.Date('2003-01-29'), as.Date('2006-01-30')), 
-                    num2 = c(20:22)) %>% 
-  mutate(datePlus1 = phytoDate + 1) %>% mutate(dateMinus1 = phytoDate - 1)
-# Use a fuzzy join to compare the two columns to the chlorophyll date
-fuzzy_left_join(chlorophyll, phytos, by = c("chloroDate" = "datePlus1", "chloroDate" = "dateMinus1"), 
-                match_fun = list(`<=`, `>=`))
 
 
