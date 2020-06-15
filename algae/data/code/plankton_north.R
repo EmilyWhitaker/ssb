86ffcab1fabfb9c2f25482ab299ae7191b87cbc8
EmilyWhitaker <ewhitaker524@gmail.com>
2020-06-15 17:58:31 -0500

    north phyto

--- algae/data/code/plankton_north.R
+++ algae/data/code/plankton_north.R
@@ -37,7 +37,7 @@ genus.ann$med.bv[is.na(genus.ann$med.bv)] <- 0
 
 tile=genus.ann
 tile = arrange(tile,taxa_name)
-tile$Genus = factor(tile$taxa_name, levels = rev(unique(tile$taxa_name)), ordered=TRUE)
+tile$TB_data = factor(tile$taxa_name, levels = rev(unique(tile$taxa_name)), ordered=TRUE)
 
 ggplot(tile, aes(x=year4,y=taxa_name,fill=med.bv))+
   geom_tile(size=0.1,na.rm=T)+
@@ -52,34 +52,33 @@ SP_data <- subset(data, lakeid== "SP")
 ggplot(SP_data, aes(as.factor(year4), log(biovolume_conc), fill=taxa_name))+
   geom_boxplot()
 
-genus.ann.sp = SP_data
-genus.ann.sp %<>% group_by(year4, taxa_name) %>%
+genus.ann = SP_data
+genus.ann %<>% group_by(year4, taxa_name) %>%
   summarize(med.bv = median(log(biovolume_conc), na.rm=T))
 
-genus.ann.sp$med.bv[is.na(genus.ann.sp$med.bv)] <- 0
+genus.ann$med.bv[is.na(genus.ann$med.bv)] <- 0
 
-tile2=genus.ann.sp
-tile2 = arrange(tile,taxa_name)
-tile2$Genus = factor(tile$taxa_name, levels = rev(unique(tile$taxa_name)), ordered=TRUE)
+tile=genus.ann
+tile = arrange(tile,taxa_name)
+tile$SP_data = factor(tile$taxa_name, levels = rev(unique(tile$taxa_name)), ordered=TRUE)
 
-ggplot(tile2, aes(x=year4,y=taxa_name,fill=med.bv))+
+ggplot(tile, aes(x=year4,y=taxa_name,fill=med.bv))+
   geom_tile(size=0.1,na.rm=T)+
   scale_fill_viridis_c(direction=-1)+
   theme_classic()
 
 
-write.csv(SP_data, 'data/SP_data_phytos.csv', row.names = F)
-
-
 # pull out genus-specific biovolumes
+TB_data_full = read.csv('data/TB_data_phytos_totals.csv', stringsAsFactors = F)
+TB_data_full %<>% select(year4, sampledate, division, taxa_name,relative_total_biovolume, genus, cells_per_nu,
+                 nu_per_ml,	cells_per_ml,	biovolume_conc,	biomass_conc)
+TB_data_full$sampledate = mdy(TB_data_full$sampledate)
+
+
+
+
 # skip ahead to clean file
 genus = subset(data, Genus != "TotalBiovolume")
 
 
-
-
-
-
-
-#make Holly's graph
 
