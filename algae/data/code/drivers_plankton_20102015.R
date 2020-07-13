bc5cb139b35536505667ee153c2b41af1a1a6bb5
EmilyWhitaker <ewhitaker524@gmail.com>
2020-07-13 11:14:11 -0500

    2010-2015 data

--- algae/data/code/drivers_plankton_20102015.R
+++ algae/data/code/drivers_plankton_20102015.R
@@ -25,9 +25,33 @@ SPZoops=read.csv('data/SPZoops.csv', stringsAsFactors = F)
 SPZoops$sample_date = ymd(SPZoops$sample_date)
 SPZoops$year= year(SPZoops$sample_date)
 SPZoops %<>% subset(year >= "2010")
+SPZoops %<>% rename(sampledate = sample_date)
+
 
 write.csv(SP_data_season_total, 'data/SP_data_season_total.csv', row.names = F)
 SPdataset = read.csv('data/joinedSPseasonFull_06172020.csv', stringsAsFactors = F)
+
+
+SPZoops %<>% select(sampledate, species_code, species_name, density, individuals_measured, avg_length) %>%
+  mutate(bv.datePlus1 = sampledate + 1) %>% mutate(bv.dateMinus1 = sampledate - 1)
+
+join_surfchlor_SP <-fuzzy_left_join(SPdataset, SPZoops, by = c("sampledate" = "bv.datePlus1", "sampledate" = "bv.dateMinus1"),
+                                    match_fun = list(`<=`, `>=`))
+
+
+completedataset=read.csv('data/joinedSPseasonFull_06172020', stringsAsFactors = F)
+
+
+write.csv(join_surfchlor_SP, 'data/joinedSPseasonFull.csv', row.names = F)
+class(join_surfchlor_SP$sampledate)
+write.csv(SP_data_season_total, 'data/SP_data_season_total.csv', row.names = F)
+joinedSPseasonFull_06172020
+
+
+
+
+
+
 
 
 ###################
