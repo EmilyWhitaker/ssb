9fb03e2c96bab1d3141764635bd7748954c42294
EmilyWhitaker <ewhitaker524@gmail.com>
2020-07-16 08:10:04 -0500

    2010-2015 data

--- algae/data/TB dataset merging .R
+++ algae/data/TB dataset merging .R
@@ -16,3 +16,35 @@ library(ggpubr); library(fuzzyjoin)
 #######
 
 #merging TB dataset-- eek 
+TB_dataphyto = read.csv('data/TB_data_phytos.csv', stringsAsFactors = F)
+TB_dataphyto$sampledate = ymd(TB_dataphyto$sampledate)
+#need to make all of the absence data
+
+
+TB_zoops= read.csv('data/TB_zoops.csv', stringsAsFactors = F)
+TB_zoops$sampledate = mdy(TB_zoops$sampledate)
+#need to make all of the absence data
+
+
+
+
+
+
+
+
+
+#totalbv_TB = read.csv("data/TotalBVs.csv", stringsAsFactors = F)
+#totalbv= read.csv("../TotalBVs.csv")
+#totalbv$sampledate = ymd(totalbv$sampledate)
+TB_data_season %<>% select(lakeid, sampledate, division, taxa_name, genus, biovolume_conc, Season) %>%
+  mutate(bv.datePlus1 = sampledate + 1) %>% mutate(bv.dateMinus1 = sampledate - 1)
+
+join_surfchlor <-fuzzy_left_join(join_ice, TB_data_season, by = c("sampledate" = "bv.datePlus1", "sampledate" = "bv.dateMinus1"),
+                                 match_fun = list(`<=`, `>=`))
+
+join_surfchlor %<>% rename(sampledate= sampledate.y)
+
+
+write.csv(join_surfchlor, 'data/joinedTBseasonFull.csv', row.names = F)
+
+class(join_surfchlor$sampledate)
